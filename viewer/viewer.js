1bbe45a90331bc08dda17f4b9476aab8bf6a704a
Kin Blas <jblas@adobe.com>
2011-05-13 17:22:57 -0700

    Added support for hover and selection.
    Externalized JS into viewer.js and added some code to pre-process the JSON data so we can work with the data easier.

---
+++ viewer/viewer.js
@@ -0,0 +1,73 @@
+function buildFunctionList(dict)
+{
+	var arr = [];
+	if (dict){
+		for (var k in dict){
+			var d = dict[k];
+			arr.push(d);
+			d.id = k;
+			d.average = Math.ceil((d.total || 0) / (d.count || 1));
+		}
+	}
+	return arr;
+}
+
+function augmentFunctionData(functionDict, callArray, callDict)
+{
+	var count = callArray.length,
+		countDict = {};
+	for (var i = 0; i < count; i++){
+		var c = callArray[i],
+			id = c.id,
+			f = functionDict[id],
+			children = c.children;
+
+		// Assign a unique id to each call so we can reference
+		// them immediately.
+
+		c.callId = countDict[id] = (countDict[id] || 0) + 1;
+
+		// Add the call to the profiled function's list
+		// of calls.
+
+		if (!f.calls){
+			f.calls = [];
+		}
+		f.calls.push(c);
+
+		// Add the call to our global call dictionary.
+
+		callDict[c.callId] = c;
+
+		if (children && children.length){
+			augmentFunctionData(functionDict, children, callDict);
+		}
+	}
+}
+
+function augmentProfilerData(pdata)
+{
+	// We need an array of the profiled functions so we can
+	// use them with data sets.
+
+	pdata.functionList = buildFunctionList(pdata.functions);
+
+	// Crawl the call graphs to build a call dictionary and associate
+	// each call with its corresponding function.
+
+	pdata.callDict = {};
+	augmentFunctionData(pdata.functions, pdata.callGraphs, pdata.callDict);
+
+	// Calculate call averages.
+	var funcs = pdata.functionList,
+		count = funcs.length;
+	for (var i = 0; i < count; i++){
+	}
+}
+
+var dsFuncs = new Spry.Data.DataSet();
+dsFuncs.setDataFromArray(buildFunctionList(gJSONData.functions));
+
+var pvFuncs = new Spry.Data.PagedView(dsFuncs, { pageSize: 20 });
+
+pvFuncs.setColumnType(["id", "min", "max", "average", "total", "count"], "number");
