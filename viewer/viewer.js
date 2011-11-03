316cb19db8e107bdee95f2924bc1455c52cd44f0
jblas@adobe.com <jblas@adobe.com>
2011-11-03 00:37:11 -0700

    First pass implementation of expandable call graph list.

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -1,78 +1,81 @@
-function buildFunctionList(dict)
+( function( $, window, undefined ) {
+
+var gProfile = null;
+
+function escapeEntities( str )
 {
-	var arr = [];
-	if (dict){
-		for (var k in dict){
-			var d = dict[k];
-			arr.push(d);
-			d.id = k;
-			d.average = Math.ceil((d.total || 0) / (d.count || 1));
+	return str.replace( /</g, "&lt;" ).replace( />/, "&gt;" ).replace( /&/, "&amp;" );
+}
+
+function createListFromArray( items, calls, arr )
+{
+	var ul = document.createElement( "ul" ),
+		callId, call, item, li, a;
+
+	for ( var i = 0; i < arr.length; i ++ ) {
+		callId = arr[ i ];
+		call = calls[ callId ];
+		if ( call ) {
+			item = items[ call[ 0 ] ];
+			li = document.createElement( "li" );
+			li.appendChild( document.createTextNode( escapeEntities( item[ 0 ] ) + " - " + call[ 3 ] ) );
+			li.setAttribute( "data-id", callId );
+			li.setAttribute( "id", "c-"+ callId );
+			ul.appendChild( li );
 		}
 	}
-	return arr;
+
+	return ul;
 }
 
-function augmentFunctionData(functionDict, callArray, callDict)
+function generateGraphs( profile )
 {
-	var count = callArray.length,
-		countDict = {};
-	for (var i = 0; i < count; i++){
-		var c = callArray[i],
-			id = c.id,
-			f = functionDict[id],
-			children = c.children;
+	gProfile = profile;
 
-		// Assign a unique id to each call so we can reference
-		// them immediately.
+	var ul = createListFromArray( profile.items, profile.calls, profile.graphs );
+	ul.setAttribute( "id", "graphList" );
 
-		c.callId = countDict[id] = (countDict[id] || 0) + 1;
+	$("#graphs").append(ul);
+}
 
-		// Add the call to the profiled function's list
-		// of calls.
+//    {
+//    	version: "0.1",
+//    
+//    	items: {
+//    		"itemId": [ label, min, max, avg, total, count ],
+//    		...
+//    	},
+//    
+//    	calls: {
+//    		"callId": [ itemId, start, stop, duration, parentCallId, [ child1CallId, child2CallId, child3CallId ... ] ],
+//    		...
+//    	},
+//    
+//    	graphs: [ "callId", "callId", "callId", ... ]
+//    }
+//
 
-		if (!f.calls){
-			f.calls = [];
-		}
-		f.calls.push(c);
-
-		// Add the call to our global call dictionary.
-
-		callDict[c.callId] = c;
-
-		if (children && children.length){
-			augmentFunctionData(functionDict, children, callDict);
-		}
+function createChildren( $li )
+{
+	var callId = $li.attr( "data-id" ),
+		calls = gProfile.calls,
+		call = calls[ callId ],
+		children = call[ 5 ];
+	if ( children && children.length ) {
+		$li.append( createListFromArray( gProfile.items, calls, children ) );
 	}
+	$li.attr( "data-loaded", "true" );
 }
 
-function augmentProfilerData(pdata)
-{
-	// We need an array of the profiled functions so we can
-	// use them with data sets.
-
-	pdata.functionList = buildFunctionList(pdata.functions);
-
-	// Crawl the call graphs to build a call dictionary and associate
-	// each call with its corresponding function.
-
-	pdata.callDict = {};
-	augmentFunctionData(pdata.functions, pdata.callgraphs, pdata.callDict);
-}
-
-// Preprocess the JSON data.
-
-augmentProfilerData(gJSONData);
-
-var dsFuncs = new Spry.Data.DataSet();
-dsFuncs.setDataFromArray(gJSONData.functionList);
-
-var pvFuncs = new Spry.Data.PagedView(dsFuncs, { pageSize: 20 });
-
-pvFuncs.setColumnType(["id", "min", "max", "average", "total", "count"], "number");
-
-Spry.Utils.addLoadListener(function(){
-	Spry.Utils.addEventListener("first-btn", "click", function(){ pvFuncs.firstPage(); });
-	Spry.Utils.addEventListener("prev-btn", "click", function(){ pvFuncs.previousPage(); });
-	Spry.Utils.addEventListener("next-btn", "click", function(){ pvFuncs.nextPage(); });
-	Spry.Utils.addEventListener("last-btn", "click", function(){ pvFuncs.lastPage(); });
+$(function() {
+	$.getJSON( "sample-json-large.js", generateGraphs );
+	$( "#graphs" ).bind( "click", function( e ) {
+		var $li = $( e.target ).closest( "li" );
+		if ( $li.length && !$li.attr( "data-loaded" ) ) {
+			createChildren( $li );
+		}
+		$li.toggleClass( "show" );
+	});
 });
+
+})( jQuery, window );
